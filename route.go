989e39cf3002144a23bd02a63373a6d7bd4bb5ea
lucarin91 <lucarin@protonmail.com>
2021-01-15 14:30:09 +0100

    feat: statistics in json format

--- route.go
+++ route.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
 )
 
 func (s *Server) redirect(w http.ResponseWriter, r *http.Request) {
-	s.updateStats(Redirect)
+	atomic.AddUint64(&s.Stats.Handlers[Redirect].Count, 1)
 
 	key := fmt.Sprint(r.URL)[1:]
 	fmt.Printf("   (debug) key: '%v'\n", key)
@@ -31,7 +32,7 @@ func (s *Server) redirect(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) shorten(w http.ResponseWriter, r *http.Request) {
-	s.updateStats(Shorten)
+	atomic.AddUint64(&s.Stats.Handlers[Shorten].Count, 1)
 
 	url := fmt.Sprint(r.URL)[9:]
 	key := s.Hasher.Hash(url)
@@ -47,24 +48,24 @@ func (s *Server) shorten(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "error: %v", e)
 		return
 	}
+	atomic.AddUint64(&s.Stats.TotalURL, 1)
 
 	fmt.Fprintf(w, "http://%v/%v", r.Host, key)
 }
 
 func (s *Server) statistics(w http.ResponseWriter, r *http.Request) {
-	s.updateStats(Statistics)
+	atomic.AddUint64(&s.Stats.Handlers[Statistics].Count, 1)
 
-	fmt.Fprintf(w, "URLs: %v\n", len(s.Kvs.storage))
-
-	fmt.Fprintf(w, "Redirect: %v\n", atomic.LoadUint64(&s.Stats.Redirects.Success))
-
-	fmt.Fprintf(w, "Handler:\n")
-	for _, v := range s.Stats.Handlers {
-		fmt.Fprintf(w, "  %v: %v\n", v.Name, atomic.LoadUint64(&v.Count))
+	if "json" == r.URL.Query().Get("format") {
+		e := json.NewEncoder(w)
+		e.SetIndent("", "  ")
+		e.Encode(s.Stats.Copy())
+	} else {
+		fmt.Fprintf(w, "URLs: %v\n", atomic.LoadUint64(&s.Stats.TotalURL))
+		fmt.Fprintf(w, "Redirect: %v\n", atomic.LoadUint64(&s.Stats.Redirects.Success))
+		fmt.Fprintf(w, "Handler:\n")
+		for _, v := range s.Stats.Handlers {
+			fmt.Fprintf(w, "  %v: %v\n", v.Name, atomic.LoadUint64(&v.Count))
+		}
 	}
-}
-
-func (s *Server) updateStats(i URLs) {
-	atomic.AddUint64(&s.Stats.ServerStats.TotalURL, 1)
-	atomic.AddUint64(&s.Stats.Handlers[i].Count, 1)
 }
--- stats.go
+++ stats.go
@@ -1,23 +1,43 @@
 package main
 
+import "sync/atomic"
+
 type Stats struct {
-	ServerStats
+	ServerStats `json:"server_stats"`
+}
+
+func (s *Stats) Copy() *Stats {
+	newS := &Stats{
+		ServerStats{
+			atomic.LoadUint64(&s.TotalURL),
+			Redirects{
+				atomic.LoadUint64(&s.Redirects.Success),
+				atomic.LoadUint64(&s.Redirects.Failed),
+			},
+			make([]Handler, len(s.Handlers)),
+		},
+	}
+
+	for i, v := range s.Handlers {
+		newS.Handlers[i] = Handler{v.Name, atomic.LoadUint64(&v.Count)}
+	}
+	return newS
 }
 
 type ServerStats struct {
-	TotalURL  uint64
-	Redirects Redirects
-	Handlers  []Handler
+	TotalURL  uint64    `json:"total_url"`
+	Redirects Redirects `json:"redirects"`
+	Handlers  []Handler `json:"handlers"`
 }
 
 type Redirects struct {
-	Success uint64
-	Failed  uint64
+	Success uint64 `json:"success"`
+	Failed  uint64 `json:"failed"`
 }
 
 type Handler struct {
-	Name  string
-	Count uint64
+	Name  string `json:"name"`
+	Count uint64 `json:"count"`
 }
 
 type URLs int
