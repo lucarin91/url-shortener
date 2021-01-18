364d2422bd0df3126684bb6d0a5d5d019cb67045
lucarin91 <lucarin@protonmail.com>
2021-01-18 13:40:49 +0100

    refactor: TestHash function

--- hasher_test.go
+++ hasher_test.go
@@ -4,18 +4,17 @@ import (
 	"testing"
 )
 
-var flagtests = []struct {
-	in  string
-	out string
-}{
-	{"about.sourcegraph.com/go/gophercon-2019-how-i-write-http-web-services-after-eight-years", "SjknVAAA"},
-	{"golang.org/doc/effective_go.html", "UAOuaAAA"},
-	{"golang.org", "R7W7LQAA"},
-}
-
 func TestHash(t *testing.T) {
+	var hashtests = []struct {
+		in  string
+		out string
+	}{
+		{"about.sourcegraph.com/go/gophercon-2019-how-i-write-http-web-services-after-eight-years", "SjknVAAA"},
+		{"golang.org/doc/effective_go.html", "UAOuaAAA"},
+		{"golang.org", "R7W7LQAA"},
+	}
 	h := NewMyHasher()
-	for _, tt := range flagtests {
+	for _, tt := range hashtests {
 		t.Run(tt.in, func(t *testing.T) {
 			v := h.Hash(tt.in)
 			if v != tt.out {
